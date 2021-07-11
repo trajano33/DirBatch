bc166f8ef18f3708c7f17c91978c4819ac84491c
Trajano157 <71413702+Trajano157@users.noreply.github.com>
2021-07-11 18:34:26 -0300

    Add files via upload

---
+++ DirBatch.bat
@@ -0,0 +1,5 @@
+@echo off
+set /p URL=Digite a URl:
+echo HTTP_CODE
+for /f "delims=" %%a in (wordlist.txt) do for /f %%b in ('curl -fIkLsw "%%{response_code}" -o NUL "%URL%/%%a"') do if not "%%b"=="404" echo %%b %URL%/%%a
+pause >nul
